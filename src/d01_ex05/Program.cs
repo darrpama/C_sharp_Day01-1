691b3e2fcf805f9fec2a9edec61006a191133708
Darr Pamala <darrpama@si-c2.kzn.21-school.ru>
2024-05-22 14:28:17 +0300

    aboba

--- src/d01_ex05/Program.cs
+++ src/d01_ex05/Program.cs
@@ -5,17 +5,37 @@ customer2.FillCart(15);
 var customer3 = new Customer("Ababa", 3);
 customer3.FillCart(15);
 
-Console.WriteLine(customer1.ToString());
-Console.WriteLine(customer2.ToString());
-Console.WriteLine(customer3.ToString());
+var customer4 = new Customer("Babab", 1);
+customer4.FillCart(15);
+var customer5 = new Customer("basse", 2);
+customer5.FillCart(15);
+var customer6 = new Customer("baefae", 3);
+customer6.FillCart(15);
 
-CashRegister register = new CashRegister("Register #1");
-register.customers.Enqueue(customer1);
-register.customers.Enqueue(customer2);
-register.customers.Enqueue(customer3);
+CashRegister register1 = new CashRegister("Register #1");
+register1.customers.Enqueue(customer1);
+register1.customers.Enqueue(customer2);
+register1.customers.Enqueue(customer3);
 
-Console.WriteLine(register.ToString());
+CashRegister register2 = new CashRegister("Register #2");
+register2.customers.Enqueue(customer4);
+register2.customers.Enqueue(customer5);
+// register2.customers.Enqueue(customer6);
 
-foreach (var customer in register.customers) {
+Console.WriteLine(register1.ToString());
+foreach (var customer in register1.customers) {
 	Console.WriteLine(customer.ToString());
 }
+
+Console.WriteLine(register2.ToString());
+foreach (var customer in register2.customers) {
+	Console.WriteLine(customer.ToString());
+}
+
+Store store = new Store(30, 2);
+store.CashRegisters.Add(register1);
+store.CashRegisters.Add(register2);
+
+var customer7 = new Customer("Andrew", 1);
+customer1.FillCart(15);
+CashRegister res = CustomerExtentions.ChooseLeastCustomers(customer7, store.CashRegisters);
